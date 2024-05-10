fc4149a1fed5e359c7b3955aa97cb2c98521303a
shunmakk <shunmakajioka12@gmail.com>
2024-05-11 03:46:58 +0900

    Headerを少し修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
 import './Header.scss';
+import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
 
 const Header = () => {
   return (
     <header className='header'>
       <div className='headerWrapper'>
         <div className='headerTitle'>
-        <Link to="/" className='Link'><h1>CarShare Review APP</h1></Link>
+        <Link to="/" className='Link'><h1>CarShare Review APP</h1><DirectionsCarIcon sx={{ fontSize: 33 , paddingLeft: 1}} color="primary" /></Link>
         </div>
         <nav className='headerNavgation'>
           <ul>
