2c2fc5e52fd54fde2c849e361bf993daa3912f75
shunmakk <shunmakajioka12@gmail.com>
2024-05-14 17:16:41 +0900

    お問い合わせページの改修

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,12 +1,13 @@
-import { Button, Container, Typography } from '@mui/material';
+import { Button, Container, FormLabel, Typography } from '@mui/material';
 import { styled } from '@mui/system';
-import { FormControl, FormHelperText, InputLabel, TextField } from '@mui/material';
+import { FormControl, FormHelperText,TextField } from '@mui/material';
 import  { useState } from 'react';
 import { useForm,SubmitHandler } from 'react-hook-form';
 import { init, send } from '@emailjs/browser';
 import toast, { Toaster } from 'react-hot-toast';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import './Contact.scss'
 
 const FormField = styled(FormControl)({
   width: '100%',
@@ -20,12 +21,14 @@ const TextArea = styled(TextField)({
 type FormValues = {
   name: string;
   email: string;
+  title: string;
   content: string;
 };
 
 const schema : yup.ObjectSchema<FormValues> = yup.object({
   name: yup.string().required('名前を入力してください').min(2, '1文字以上で入力してください').max(15, '15文字以下で入力してください'),
   email: yup.string().required('メールアドレスを入力してください').email('メールアドレスの形式ではありません'),
+  title: yup.string().required(),
   content: yup.string().required('1文字以上で入力してください'),
 });
 
@@ -38,6 +41,7 @@ const Contact = () => {
     defaultValues: {
       name: '',
       email: '',
+      title: '',
       content: '',
     },
   });
@@ -47,7 +51,7 @@ const Contact = () => {
     const serviceId = process.env.REACT_APP_SERVICE_ID;
     const templateId = process.env.REACT_APP_TEMPLATE_ID;
   
-    const { name, email, content } = data;
+    const { name, email,title, content } = data;
   
     if (userId && serviceId && templateId) {
       setIsSending(true);
@@ -58,6 +62,7 @@ const Contact = () => {
       const params = {
         name: name,
         email: email,
+        title: title,
         content: content,
         from: 'カーシェアレビューアップ',
       };
@@ -81,21 +86,21 @@ const Contact = () => {
     }
   };
   return (
-    <Container maxWidth="md" sx={{ height: '100vh',  paddingTop: '50px',
+    <Container maxWidth="md" sx={{ overflowY: 'scroll',  paddingTop: '50px', paddingBottom: '30px'
     }}>
       <Toaster />
-      <div className='Form' style={{backgroundColor: 'white', padding: '50px'}}>
+      <div style={{ backgroundColor: 'white', padding: '50px', borderRadius: '30px', boxShadow: '0px 0px 18px -5px #777777' }}>
         <Typography variant="h4" component="h2" sx={{ mb: 5 ,textAlign: 'center'}}>
           お問い合わせフォーム
         </Typography>
         <form onSubmit={handleSubmit(onSubmit)} style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-          <FormField>
-            <InputLabel htmlFor="name"></InputLabel>
-            <TextField id="name" placeholder="山田太郎"  {...control.register('name')} disabled={isSending} />
+          <FormField  >
+            <FormLabel htmlFor="name">名前</FormLabel>
+            <TextField id="name" placeholder="山田太郎"  {...control.register('name')} disabled={isSending}  />
             <FormHelperText>名前を入力してください</FormHelperText>
           </FormField>
           <FormField>
-            <InputLabel htmlFor="email"></InputLabel>
+            <FormLabel  htmlFor="email">メールアドレス</FormLabel>
             <TextField
               id="email"
               placeholder="exaple@yahoo.co.jp"
@@ -106,7 +111,17 @@ const Contact = () => {
             <FormHelperText>メールアドレスを入力してください</FormHelperText>
           </FormField>
           <FormField>
-            <InputLabel htmlFor="content"></InputLabel>
+            <FormLabel  htmlFor="title">件名</FormLabel>
+            <TextField
+              id="title"
+              placeholder="件名を入力"
+              {...control.register('title')}
+              disabled={isSending}
+            />
+            <FormHelperText>件名を入力してください</FormHelperText>
+          </FormField>
+          <FormField>
+            <FormLabel  htmlFor="content">本文</FormLabel >
             <TextArea
               id="content"
               placeholder="お問い合わせ内容について入力"
